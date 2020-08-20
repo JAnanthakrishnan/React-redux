bb9d29d77af0077a12f8ca6422e1018004f882f5
JAnanthakrishnan <ananthakrishnan2017@gmail.com>
2020-08-20 15:28:16 +0530

    Introducing react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Homepage from './components/home/HomePage';
+import AboutPage from './components/about/AboutPage';
+import Header from './components/common/Header';
+import PageNotFound from './components/PageNotFound';
+import CoursesPage from './components/courses/CoursesPage';
 import './App.css';
 
 function App() {
-  return <Router></Router>;
+  return (
+    <div id='app'>
+      <Router>
+        <div className='container-fluid'>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={Homepage} />
+            <Route path='/courses' component={CoursesPage} />
+            <Route path='/about' component={AboutPage} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class CoursesPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Courses</h2>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
