fff70bc19ffd6199491b5c1226fa30b9cbe13005
JAnanthakrishnan <ananthakrishnan2017@gmail.com>
2020-08-21 09:29:30 +0530

    crated mock api

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../redux/actions/courseActions';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
 
 export class CoursesPage extends Component {
+  state = {
+    course: {
+      title: ''
+    }
+  };
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({
+      course
+    });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.actions.createCourse(this.state.course);
+  };
   render() {
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
-      </div>
+        <h3>Add Course</h3>
+        <input
+          type='text'
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type='submit' value='Save' />
+        {this.props.courses.map((course) => (
+          <div id='course.title'>{course.title}</div>
+        ))}
+      </form>
     );
   }
 }
-
-export default CoursesPage;
+CoursesPage.propTypes = {
+  actions: PropTypes.object.isRequired
+};
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+// !third way of dispatching actions
+function mapDispatchToProps(dispatch) {
+  return {
+    //! createCourse: (course) => dispatch(courseActions.createCourse(course)) //Manual way of updating
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+// !object form of dispatching actions (automatically dispatched by connect)
+// const mapDispatchToProps = {
+//   createCourse: courseActions.createCourse
+// };
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import configureStore from './redux/configureStore';
+const store = configureStore();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
